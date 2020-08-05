cb1d2c8532ff46d2280c3590a7b1850eaeb0999c
1-4200 <tkt193yk@gmail.com>
2020-08-05 22:27:53 +0900

    fix: hot load error

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -5,14 +5,10 @@ const StartServerPlugin = require('start-server-webpack-plugin')
 
 module.exports = {
     entry: [
-        'webpack/hot/poll?1000',
         './src/server/index'
     ],
     watch: true,
     target: 'node',
-    externals: [nodeExternals({
-        whitelist: ['webpack/hot/poll?1000']
-    })],
     module: {
         rules: [{
             test: /\.js?$/,
@@ -23,14 +19,17 @@ module.exports = {
     plugins: [
         new StartServerPlugin('server.js'),
         new webpack.NamedModulesPlugin(),
-        new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin(),
         new webpack.DefinePlugin({
             "process.env": {
                 "BUILD_TARGET": JSON.stringify('server')
             }
         }),
+        new webpack.IgnorePlugin(/^electron$/)
     ],
+    externals: {
+        electron: "electron",
+    },
     output: {
         path: path.join(__dirname, 'prod/server'),
         filename: 'server.js'
