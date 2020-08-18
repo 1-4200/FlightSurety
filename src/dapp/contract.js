36fbaaaa02f63a91aa791949379ade9c27e7e28a
1-4200 <tkt193yk@gmail.com>
2020-08-18 23:37:00 +0900

    fix: change protocol to ws

--- src/dapp/contract.js
+++ src/dapp/contract.js
@@ -4,9 +4,8 @@ import Web3 from 'web3';
 
 export default class Contract {
     constructor(network, callback) {
-
         let config = Config[network];
-        this.web3 = new Web3(new Web3.providers.HttpProvider(config.url));
+        this.web3 = new Web3(new Web3.providers.WebsocketProvider(config.url));
         this.flightSuretyApp = new this.web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
         this.initialize(callback);
         this.owner = null;
