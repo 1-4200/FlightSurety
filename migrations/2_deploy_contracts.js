ed4992308e1d45167563a07204c94e0b544234d2
1-4200 <tkt193yk@gmail.com>
2020-08-19 00:14:31 +0900

    fix: deploy contract script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,25 +3,23 @@ const FlightSuretyData = artifacts.require("FlightSuretyData");
 const fs = require('fs');
 const Web3 = require('web3');
 
-module.exports = function (deployer, network, accounts) {
-    if (network === "develop") {
-        let web3 = new Web3(new Web3.providers.WebsocketProvider('ws://127.0.0.1:8545'));
-        let fund = web3.utils.toWei('10', 'ether')
-        let owner = accounts[0];
-        deployer.deploy(FlightSuretyData, {from: owner, value: fund})
-            .then(() => {
-                return deployer.deploy(FlightSuretyApp, FlightSuretyData.address, {from: owner})
-                    .then(() => {
-                        let config = {
-                            localhost: {
-                                url: 'http://localhost:8545',
-                                dataAddress: FlightSuretyData.address,
-                                appAddress: FlightSuretyApp.address
-                            }
+module.exports = function (deployer) {
+
+    let web3 = new Web3(new Web3.providers.WebsocketProvider('ws://127.0.0.1:8545'));
+    let fund = web3.utils.toWei('10', 'ether')
+    deployer.deploy(FlightSuretyData, {value: fund})
+        .then(() => {
+            return deployer.deploy(FlightSuretyApp, FlightSuretyData.address)
+                .then(() => {
+                    let config = {
+                        localhost: {
+                            url: 'http://localhost:8545',
+                            dataAddress: FlightSuretyData.address,
+                            appAddress: FlightSuretyApp.address
                         }
-                        fs.writeFileSync(__dirname + '/../src/dapp/config.json', JSON.stringify(config, null, '\t'), 'utf-8');
-                        fs.writeFileSync(__dirname + '/../src/server/config.json', JSON.stringify(config, null, '\t'), 'utf-8');
-                    });
-            });
-    }
+                    }
+                    fs.writeFileSync(__dirname + '/../src/dapp/config.json', JSON.stringify(config, null, '\t'), 'utf-8');
+                    fs.writeFileSync(__dirname + '/../src/server/config.json', JSON.stringify(config, null, '\t'), 'utf-8');
+                });
+        });
 }
